ed3fa0efefac752bc5b1048311d93c131c7058ca
Ing. Jakub Jirák <jirak92@gmail.com>
2021-05-25 20:12:18 +0200

    Add custom routing (correct for React, prevent rerender page)

---
+++ src/components/Link.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Link = ({className, href, children}) => {
+    const onClick = (event) => {
+        event.preventDefault();
+        window.history.pushState({}, '', href);
+
+        //This will communicate over route component that route just change
+        const navigationEvent = new PopStateEvent('popstate');
+        window.dispatchEvent(navigationEvent);
+
+    }
+    return (
+        <a
+            onClick={onClick}
+            className={className}
+            href={href}>
+                {children}
+        </a>
+    )
+};
+
+export default Link;
